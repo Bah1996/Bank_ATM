1fef9fac1666b51931ff866805e74dca3f5a8974
Bah1996 <90456889+Bah1996@users.noreply.github.com>
2025-07-03 09:38:25 +0500

    Initial commit

---
+++ src/main/java/uz/bax/bankatmcontrolsystem/entity/enums/RoleName.java
@@ -0,0 +1,8 @@
+package uz.bax.bankatmcontrolsystem.entity.enums;
+
+public enum RoleName {
+
+    ADMIN,
+    BANK_DIRECTOR,
+    BANK_EMPLOYEE
+}
---
+++ src/main/java/uz/bax/bankatmcontrolsystem/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package uz.bax.bankatmcontrolsystem.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.bek.bankatmcontrolsystem.entity.Role;
+import uz.bek.bankatmcontrolsystem.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<Role, UUID> {
+
+    Optional<User> findByEmail(String username);
+
+    Optional<User> findByEmailAndEmailCode(String email, String emailCode);
+}
